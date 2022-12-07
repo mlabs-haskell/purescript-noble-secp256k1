5c2c946781e82f10688c52067e4d50240366ddb7
Amir Rad <ahmr10@gmail.com>
2022-12-07 15:51:57 +0000

    Fix incomplete ord foreign fn

--- src/Noble/Internal/Helpers.js
+++ src/Noble/Internal/Helpers.js
@@ -2,11 +2,12 @@ exports.byteLength = bytes => bytes.byteLength;
 
 exports._ordIntArray = f => xs => ys => {
   for (let i = 0; i < xs.length && i < ys.length; i++) {
-    if (xs[i] !== ys[i]) {
-      return xs[i] < ys[i] ? -1 : 1;
+    let o = f(xs[i])(ys[i]);
+    if (o !== 0) {
+      return o
     }
   }
   return xs.length === ys.length ? 0 : xs.length > ys.length ? -1 : 1;
 };
 
-exports.showBytes = bytes => "[ " + bytes.join(", ") + " ]";
+exports.showBytes = bytes => "[" + bytes.join(", ") + "]";
